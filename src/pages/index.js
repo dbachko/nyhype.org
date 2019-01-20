5a67e7a4ff050f49b4910917fffbaa69faefda07
dbachko <dbachko@gmail.com>
2019-01-20 13:22:02 -0500

    Small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>¡Hola, amigos!</h1>
+    <p>Welcome to our brand new Supreme crypto store.</p>
+    <p>Now you can buy some hype staff with BTC, LTC or even ETH.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
