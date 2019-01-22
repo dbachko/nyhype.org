6bb950eb498e29df999ead066dbc3b2ca6261de5
dbachko <dbachko@gmail.com>
2019-01-21 21:11:47 -0500

    Test func 2

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -4,10 +4,31 @@ const { CB_COMMERCE_API_KEY } = process.env
 const API_ENDPOINT = 'https://api.commerce.coinbase.com/checkouts'
 
 exports.handler = async () => {
+  const json = await fetch(API_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CC-Api-Key': CB_COMMERCE_API_KEY,
+      'X-CC-Version': '2018-03-22',
+    },
+    body: JSON.stringify({
+      name: 'The Sovereign Individual',
+      description: 'Mastering the Transition to the Information Age',
+      local_price: {
+        amount: '100.00',
+        currency: 'USD',
+      },
+      pricing_type: 'fixed_price',
+      requested_info: ['email'],
+    }),
+  }).then(response => response.json())
+
+  console.log(json)
+
   return {
     statusCode: 200,
-    body: CB_COMMERCE_API_KEY
-  };
+    body: json,
+  }
   // return fetch(API_ENDPOINT, {
   //   method: 'POST',
   //   headers: {
