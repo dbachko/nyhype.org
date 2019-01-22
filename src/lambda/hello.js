eb30a619e95802fbb43e9bb75d811cd400eb634c
dbachko <dbachko@gmail.com>
2019-01-21 21:24:30 -0500

    Adds proper charge function

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,56 +1,40 @@
 import fetch from 'isomorphic-fetch'
 
 const { CB_COMMERCE_API_KEY } = process.env
-const API_ENDPOINT = 'https://api.commerce.coinbase.com/checkouts'
+const API_ENDPOINT = 'https://api.commerce.coinbase.com/charges'
 
 exports.handler = async () => {
-  const json = await fetch(API_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CC-Api-Key': CB_COMMERCE_API_KEY,
-      'X-CC-Version': '2018-03-22',
-    },
-    body: JSON.stringify({
-      name: 'The Sovereign Individual',
-      description: 'Mastering the Transition to the Information Age',
-      local_price: {
-        amount: '100.00',
-        currency: 'USD',
+  try {
+    // Create checkout.
+    const json = await fetch(API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CC-Api-Key': CB_COMMERCE_API_KEY,
+        'X-CC-Version': '2018-03-22',
       },
-      pricing_type: 'fixed_price',
-      requested_info: ['email'],
-    }),
-  }).then(response => response.json())
-
-  console.log(json)
-
-  return {
-    statusCode: 200,
-    body: json,
+      body: JSON.stringify({
+        name: 'The Sovereign Individual',
+        description: 'Mastering the Transition to the Information Age',
+        local_price: {
+          amount: '100.00',
+          currency: 'USD',
+        },
+        pricing_type: 'fixed_price',
+        requested_info: ['email'],
+      }),
+    }).then(response => response.json())
+    // Get values from response.
+    const { hosted_url: url } = json
+    // Return url to checkout.
+    return {
+      statusCode: 200,
+      body: url,
+    }
+  } catch (e) {
+    return {
+      statusCode: 422,
+      body: String(e),
+    }
   }
-  // return fetch(API_ENDPOINT, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'X-CC-Api-Key': CB_COMMERCE_API_KEY,
-  //     'X-CC-Version': '2018-03-22',
-  //   },
-  //   body: JSON.stringify({
-  //     name: 'The Sovereign Individual',
-  //     description: 'Mastering the Transition to the Information Age',
-  //     local_price: {
-  //       amount: '100.00',
-  //       currency: 'USD',
-  //     },
-  //     pricing_type: 'fixed_price',
-  //     requested_info: ['email'],
-  //   }),
-  // })
-  //   .then(response => response.json())
-  //   .then(data => ({
-  //     statusCode: 200,
-  //     body: data,
-  //   }))
-  //   .catch(error => ({ statusCode: 422, body: String(error) }))
 }
